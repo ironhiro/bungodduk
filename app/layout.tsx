0bd9b1663574cd20ae862a8063158aa21e3a376f
ironhiro <iuncehiro@gmail.com>
2025-08-26 00:20:50 +0900

    Fixed: Meta Data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Black_Han_Sans } from "next/font/google";
 
 const blackHanSans = Black_Han_Sans({ weight:"400", subsets:["latin"], display:"swap", variable:"--font-black-han-sans" });
 
-export const metadata: Metadata = { title:"Bung Odduk Cubes", description:"3D cube demo (R3F)" };
+export const metadata: Metadata = { title:"붕오떡", description:"붕 오 떡\n어   볶\n빵 뎅 이" };
 
 export default function RootLayout({ children }: { children: React.ReactNode }){
   return (
